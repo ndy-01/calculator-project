323aa055188f06a2a799f959af7f9e79d4cf40cb
ndy-01 <stuart@lizardland.co.uk>
2019-01-21 21:56:48 +0000

    Symbol checking fixed. Function added to limit the display to 8 characters.

--- calc.js
+++ calc.js
@@ -4,32 +4,70 @@ document.addEventListener('keydown', function(e){
 });
 
 document.addEventListener('keyup', function(e){
-    let key = e.key;
-    let button = document.getElementById(key.toString());
+    let button = document.getElementById(e.key);
     button.classList.toggle('boxpressed');
     button.classList.toggle('box');
-    //console.log(button);
 });
 
+function dealWithKeyPress(event){
+    let key = getKeyPressed(event.key);
+    if (key === false){
+        alert("not a symbol");
+        return};
+    if (displayTooLong()) {
+        return;
+    };
+
+
+    shoogleTheBox(key);
+    addToDisplay(key);
+}
+
+function displayTooLong(){
+    var numDisplay = document.getElementById('numDisplay');
+    var strDisplay = numDisplay.innerText;
+    return (strDisplay.length < 8 ? false : true);
+}
+
 function getKeyPressed(eventKey){
-    let symbolList = ['+', '-', '/', '*', '=', '.'];
+    let symbolList = ['+', '-', '/', '*', '=', '.', 'Shift'];
+    let idList = ['plus', 'minus', 'divide', 'multiply', 'equals', 'decimal', 'shift'];
     let answer = false;
     if (eventKey >= 0 && eventKey <=9){
         answer = eventKey;
     } else for (let i = 0, l = symbolList.length; i < l; i++){
         if (symbolList[i] == eventKey){
+            alert('is a symbol');
             answer = symbolList[i];
         }
     }
     return answer;
 }
 
+function shoogleTheBox(boxID){
+    let button = document.getElementById(boxID.toString());
+    button.classList.toggle('boxpressed');
+    button.classList.toggle('box');
+}
+
 function addToDisplay(k){
     if (isKeyASymbol(k)){
-
+        itsASymbol(k);
     } else {
         itsANumber(k);
     }
+}
+
+function isKeyASymbol(key){
+    if (key >= 0 && key <=9){
+        return false;
+    } else {
+        return true;
+    }
+}
+
+function itsASymbol(x){
+    let symbol = x.key;
 }
 
 function itsANumber(key){
@@ -39,22 +77,5 @@ function itsANumber(key){
         dispBox.innerText = key.toString();
     } else {
         dispBox.innerText += key.toString();
-    }
-}
-
-function dealWithKeyPress(event){
-    console.log(event);
-    let key = getKeyPressed(event.key);
-    let button = document.getElementById(key.toString());
-    button.classList.toggle('boxpressed');
-    button.classList.toggle('box');
-    addToDisplay(key);
-}
-
-function isKeyASymbol(key){
-    if (key >= 0 && key <=9){
-        return false;
-    } else {
-        return true;
     }
 }
